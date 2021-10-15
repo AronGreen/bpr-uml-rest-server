be2fcf44f49a2a44bfa07585a39da5710517f607
mementomori98 <sebestyen.mate98@gmail.com>
2021-10-15 11:57:58 +0200

    wip(repo)

--- src/repository.py
+++ src/repository.py
@@ -9,32 +9,22 @@ class Collection(Enum):
     WORKSPACE = "workspace"
     USER = "user"
     APPLICATION_LOG = 'application_log'
+    MATE_TEST = 'mate_test'
 
 
-def insert_one(item, collection: Collection):
-    col = __get_collection(collection)
-    return col.insert_one(item)
+def insert(collection: Collection, item):
+    return __get_collection(collection).insert_one(item)
 
 
-def find_one(id, collection: Collection):
-    col = __get_collection(collection)
-    return col.find_one( { "_id": ObjectId(id) } )
-
-# TODO: look into **kwargs in stead of filter
-def find_one_with_filter(id, filter, collection: Collection):
-    col = __get_collection(collection)
-    return col.find_one( { "_id": ObjectId(id) }, filter )
+def find(collection: Collection, **filter):
+    return __get_collection(collection).find(filter)
 
 
-def find(filter, collection: Collection):
-    col = __get_collection(collection)
-    if filter is None:
-        return col.find()
-    return col.find(filter)
+def find_one(collection: Collection, **filter):
+    return __get_collection(collection).find_one(filter)
 
 
 def __get_collection(collection: Collection):
     client = mongo.MongoClient(f'{settings.MONGO_PROTOCOL}://{settings.MONGO_USER}:{settings.MONGO_PW}@{settings.MONGO_HOST}/{settings.MONGO_DEFAULT_DB}?retryWrites=true&w=majority')
     db = client.get_default_database()
     return db[collection.value]
-
