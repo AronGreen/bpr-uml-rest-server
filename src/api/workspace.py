47a5042988506f8d735f938ec6448b4a3a3e1bd0
Aron Wissing Kjærgaard <agk1304@gmail.com>
2021-12-07 10:02:23 +0100

    fix swagger and reformat whitespace

--- src/api/workspace.py
+++ src/api/workspace.py
@@ -240,7 +240,8 @@ def invite_user():
           description: Invitation flow is started for user
       """
     request_data = request.get_json()
-    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(request_data['workspaceId']), permissions=[WorkspacePermission.MANAGE_WORKSPACE])
+    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(request_data['workspaceId']),
+                                         permissions=[WorkspacePermission.MANAGE_WORKSPACE])
     if 'workspaceId' in request_data and 'inviteeEmailAddress' in request_data:
         invitation = Invitation(
             _id=None,
@@ -278,11 +279,12 @@ def remove_user_from_workspace():
           description: User is removed
       """
     request_data = request.get_json()
-    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(request_data['workspaceId']), permissions=[WorkspacePermission.MANAGE_WORKSPACE])
+    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(request_data['workspaceId']),
+                                         permissions=[WorkspacePermission.MANAGE_WORKSPACE])
     if 'workspaceId' in request_data and 'userId' in request_data:
-      return Response(ApiResponse(workspace_service.remove_workspace_user(
-        ObjectId(request_data['workspaceId']),
-        ObjectId(request_data['userId']))).as_json(), status = 200, mimetype="application/json")
+        return Response(ApiResponse(workspace_service.remove_workspace_user(
+            ObjectId(request_data['workspaceId']),
+            ObjectId(request_data['userId']))).as_json(), status=200, mimetype="application/json")
     return abort(400, descriptioin="Workspace id and/or user id missing")
 
 
@@ -314,8 +316,10 @@ def respond_to_invitation():
     if 'invitationId' in request_data and 'accepted' in request_data:
         invitation_id = request_data['invitationId']
         accepted = request_data['accepted']
-        return Response(status=200, response=ApiResponse(response=workspace_service.respond_to_invitation(invitation_id, accepted)).as_json())
+        return Response(status=200, response=ApiResponse(
+            response=workspace_service.respond_to_invitation(invitation_id, accepted)).as_json())
     return Response(status=400, response=ApiResponse(response="Insufficient data").as_json())
+
 
 @api.route("/<workspaceId>/teams", methods=['GET'])
 def get_workspace_teams(workspaceId: str):
@@ -334,7 +338,9 @@ def get_workspace_teams(workspaceId: str):
         404:
           description: workspace not found
       """
-    return Response(status=200, response=Team.as_json_list(workspace_service.get_teams(workspace_id=ObjectId(workspaceId), firebase_id=g.firebase_id)), mimetype="application/json")
+    return Response(status=200, response=Team.as_json_list(
+        workspace_service.get_teams(workspace_id=ObjectId(workspaceId), firebase_id=g.firebase_id)),
+                    mimetype="application/json")
 
 
 @api.route("/<workspaceId>", methods=['PUT'])
@@ -362,16 +368,18 @@ def update_workspace_name(workspaceId: str):
         404:
           description: workspace not found
       """
-    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(workspaceId), permissions=[WorkspacePermission.MANAGE_WORKSPACE])
+    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(workspaceId),
+                                         permissions=[WorkspacePermission.MANAGE_WORKSPACE])
     request_data = request.get_json()
     if 'name' in request_data:
         name = request_data['name']
-    return Response(status=200, response=Workspace.as_json(workspace_service.update_workspace_name(workspace_id=workspaceId, name=name)), mimetype="application/json")
+    return Response(status=200, response=Workspace.as_json(
+        workspace_service.update_workspace_name(workspace_id=workspaceId, name=name)), mimetype="application/json")
 
-  
+
 @api.route("/<workspaceId>", methods=['DELETE'])
 def delete_workspace(workspaceId: str):
-  """
+    """
       Delete workspace
       ---
       tags:
@@ -384,12 +392,15 @@ def delete_workspace(workspaceId: str):
         200:
           description: confirmation
       """
-  permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(workspaceId), permissions=[WorkspacePermission.MANAGE_WORKSPACE])
-  return Response(status=200, response=ApiResponse(workspace_service.delete_workspace(workspace_id=ObjectId(workspaceId))).as_json(), mimetype="application/json")
+    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(workspaceId),
+                                         permissions=[WorkspacePermission.MANAGE_WORKSPACE])
+    return Response(status=200, response=ApiResponse(
+        workspace_service.delete_workspace(workspace_id=ObjectId(workspaceId))).as_json(), mimetype="application/json")
+
 
 @api.route("/<workspaceId>/<userId>/permissions", methods=['PUT'])
 def edit_workspace_permissions_for_user(workspaceId, userId):
-  """
+    """
       Update a user's workspace permissions
       ---
       tags:
@@ -415,13 +426,19 @@ def edit_workspace_permissions_for_user(workspaceId, userId):
         404:
           description: workspace not found
       """
-  permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(workspaceId), permissions=[WorkspacePermission.MANAGE_PERMISSIONS])
-  request_data = request.get_json()
-  return Response(status=200, response=workspace_service.update_user_permissions(workspace_id = ObjectId(workspaceId), user_id = ObjectId(userId), permissions=request_data["permissions"]).as_json(), mimetype="application/json")
+    permission_service.check_permissions(firebase_id=g.firebase_id, workspace_id=ObjectId(workspaceId),
+                                         permissions=[WorkspacePermission.MANAGE_PERMISSIONS])
+    request_data = request.get_json()
+    return Response(status=200, response=workspace_service.update_user_permissions(workspace_id=ObjectId(workspaceId),
+                                                                                   user_id=ObjectId(userId),
+                                                                                   permissions=request_data[
+                                                                                       "permissions"]).as_json(),
+                    mimetype="application/json")
+
 
 @api.route("/<workspaceId>/user", methods=['GET'])
 def get_workspace_user(workspaceId):
-  """
+    """
       Get workspace user
       ---
       tags:
@@ -434,6 +451,9 @@ def get_workspace_user(workspaceId):
         200:
           description: workspace user
         404:
-          workspace not found
+          description: workspace not found
       """
-  return Response(status=200, response=workspace_service.get_workspace_user(firebase_id=g.firebase_id, workspace_id=ObjectId(workspaceId)).as_json(), mimetype="application/json")
+    return Response(status=200,
+                    response=workspace_service.get_workspace_user(firebase_id=g.firebase_id,
+                                                                  workspace_id=ObjectId(workspaceId)).as_json(),
+                    mimetype="application/json")
